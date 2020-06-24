e7753532c9e809478c8e6bf4776c3be6d38b24ed
nsuarez99 <ngsuarez99@gmail.com>
2020-06-24 16:31:25 -0400

    connected and parcing succesffully to now playing api

--- app/src/main/java/com/example/flixster/MainActivity.java
+++ app/src/main/java/com/example/flixster/MainActivity.java
@@ -7,6 +7,13 @@ import android.util.Log;
 
 import com.codepath.asynchttpclient.AsyncHttpClient;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.example.flixster.models.Movie;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
 
 import okhttp3.Headers;
 
@@ -14,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
     public static final String TAG = "MainActivity";
+    List<Movie> movies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +32,18 @@ public class MainActivity extends AppCompatActivity {
         client.get(NOW_PLAYING_URL, new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Headers headers, JSON json) {
-                Log.i(TAG, "onSucess");
+                Log.d(TAG, "onSucess");
+                JSONObject jsonObject = json.jsonObject;
+                try{
+                    JSONArray results = jsonObject.getJSONArray("results");
+                    Log.i(TAG, "Results: " + results.toString());
+                    List<Movie> movies = Movie.fromJSONArray(results);
+                    Log.i(TAG, "Movies: " + movies.size());
+
+                }
+                catch (JSONException e){
+                    Log.e(TAG, "Hit json exception no results found");
+                }
             }
 
             @Override
