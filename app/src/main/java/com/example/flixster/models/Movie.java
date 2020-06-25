b56ee955fe7231cbcf986debacb4d0ff21ad57fd
nsuarez99 <ngsuarez99@gmail.com>
2020-06-25 16:30:02 -0400

    working shift from portrait to landscape

--- app/src/main/java/com/example/flixster/models/Movie.java
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -10,11 +10,13 @@ import java.util.List;
 public class Movie {
 
     String posterPath;
+    String backdropPath;
     String title;
     String overview;
 
     public Movie(JSONObject jsonObject) throws JSONException {
         posterPath = jsonObject.getString("poster_path");
+        backdropPath = jsonObject.getString("backdrop_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
     }
@@ -27,6 +29,10 @@ public class Movie {
         return movies;
     }
 
+
+    public String getBackdropPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s", backdropPath);
+    }
     public String getPosterPath() {
         return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath);
     }
